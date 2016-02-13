65c1c9991791b69ab3bf1636c3dafc71976b6433
mh35 <mh35jp@gmail.com>
2016-02-13 19:33:21 +0900

    Update

--- greet-time-name.php
+++ greet-time-name.php
@@ -16,8 +16,6 @@ if(!function_exists('greet_time_name_plugins_loaded')) {
   function greet_time_name_plugins_loaded() {
     load_plugin_textdomain('greet-time-name', false, basename(
       dirname(__FILE__)) . '/languages');
-    remove_action('admin_bar_menu', 'wp_admin_bar_my_account_item', 7);
-    add_action('admin_bar_menu', 'greet_time_name_add_menu_item', 7);
   }
 }
 add_action('plugins_loaded', 'greet_time_name_plugins_loaded');
@@ -32,7 +30,7 @@ if(!function_exists('greet_time_name_add_menu_item')) {
     $profile_url = get_edit_profile_url($user_id);
     $avatar_size = apply_filters('greet-time-name_avatar_size', 26);
     $avatar = get_avatar($user_id, $avatar_size);
-    $time = date_i18n('G', time());
+    $time = date_i18n('G', time() + get_option('gmt_offset') * HOUR_IN_SECONDS);
     $time = (int)$time;
     if($time >= 6 && $time < 12) {
       $howdy = sprintf(__('Good morning, %s', 'greet-time-name'),
@@ -57,3 +55,11 @@ if(!function_exists('greet_time_name_add_menu_item')) {
     ));
   }
 }
+
+if(!function_exists('greet_time_name_admin_bar_menus')) {
+  function greet_time_name_admin_bar_menus() {
+    remove_action('admin_bar_menu', 'wp_admin_bar_my_account_item', 7);
+    add_action('admin_bar_menu', 'greet_time_name_add_menu_item', 7);
+  }
+}
+add_action('add_admin_bar_menus', 'greet_time_name_admin_bar_menus');
